efadece7a6cd34e61d28337764f6b808e823fdb7
Vsevolod Rodionov <crbrus@gmail.com>
2012-11-28 15:09:31 +0400

    22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ L2013::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false # todo undo
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
