9a5940afddcfb545f237fdec1cf635c7c7ff1b33
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-17 12:24:48 +0400

    22

--- script/parser.rb
+++ script/parser.rb
@@ -8,17 +8,17 @@ Bluepill.application 'lipstagram' do |app|
     process.daemonize = true
     process.checks :cpu_usage, every: 5.seconds, below: 10, times: [7,10]
     process.checks :mem_usage, every: 5.seconds, below: 100.megabytes, times: [7,10]
-    process.stop_signals = [:quit, 30.seconds, :term, 5.seconds, :kill]
   end
   app.process 'instagram_parser' do |process|
     process.start_command = 'ruby script/instagram.rb start'
     process.pid_file = '/tmp/instagram_parser.pid'
     process.daemonize = true
-    process.checks :cpu_usage, every: 5.seconds, below: 10, times: 3
+    process.checks :cpu_usage, every: 5.seconds, below: 10, times: [7,10]
+    process.checks :mem_usage, every: 5.seconds, below: 100.megabytes, times: [7,10]
   end
-  app.process 'twitter_parser' do |process|
-    process.start_command = 'thin start -p 80 -e production'
-    process.pid_file = '/tmp/twitter_parser.pid'
+  app.process 'server' do |process|
+    process.start_command = 'thin start -p 80 -e production -d'
+    process.pid_file = '/tmp/thin.pid'
     process.daemonize = true
   end
 end
