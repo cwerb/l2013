ea5206e18e619a52e68e49333c00a61aec3b8200
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-19 07:22:02 +0400

    22

--- app/models/auth.rb
+++ app/models/auth.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Auth < ActiveRecord::Base
   scope :registred, -> {where('"email" NOTNULL')}
-  attr_accessible :provider, :uid, :name, :data, :url, :images, :is_subscribed, :accepted_deal, :images, :own_images, :avatar_url
+  attr_accessible :provider, :uid, :name, :data, :url, :images, :is_subscribed, :accepted_deal, :images, :own_images, :avatar_url, :user
+  belongs_to :user
   has_many :own_images, class_name: :Image, foreign_key: :author_id
   serialize :data
   has_and_belongs_to_many :images, uniq: true
