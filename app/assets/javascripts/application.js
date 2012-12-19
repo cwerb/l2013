d1f4e0ea421a7b6f91f26a2e94d3ad1f3ef27585
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-19 06:50:20 +0400

    22

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,28 +16,3 @@
 //= require_self
 
 //transition scroll
-
-
-function set_size( elem, w_const, h_const )
-{
-    if ( typeof h_const == 'undefined' ) h_const = w_const;
-
-    var h = elem.height();
-    var w = elem.width();
-
-    var k = h/w;
-    var w_ = w_const;
-    var h_ = w_*k;
-    if ( h_ < h_const )
-    {
-        h_ = h_const;
-        w_ = h_/k;
-    }
-
-    elem.css({
-        position : 'absolute',
-        width: w_,
-        height: h_,
-        margin: (h_const-h_)/2+'px 0 0 '+(w_const-w_)/2+'px'
-    });
-}
